b02d25be6e4212ba2dba1e079e800ada7e3fe4ee
Arsen-Git <asermahambetov@gmail.com>
2022-10-23 15:33:26 +0200

    add increase button functionality

--- src/components/app-employees-list-item/app-employees-list-item.js
+++ src/components/app-employees-list-item/app-employees-list-item.js
@@ -1,16 +1,35 @@
 import "./app-employee-list-item.css"
+import { Component } from "react";
 
-const AppEmployeeItem = ({name,salary}) => {
-    return(
-        <li className="employee__container">
-            <p className="employee__name">{name}</p>
-            <p className="employee__salary">{salary + "$"}</p>
-            <div className="employee__btns">
-                <button className="btn btn__increase">Increase</button>
-                <button className="btn btn__delete">Delete</button>
-            </div>
-        </li>
-    )
+class AppEmployeeItem extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            increase: false
+        }
+    }
+
+    onChange = () => {
+        this.setState(({increase}) => ({
+            increase: !increase
+        }))
+    }
+
+    render(){
+        const {name, salary} = this.props;
+        const {increase} = this.state;
+        const className = increase?"employee__container increase":"employee__container";
+        return(
+            <li className={className}>
+                <p className="employee__name">{name}</p>
+                <p className="employee__salary">{salary + "$"}</p>
+                <div className="employee__btns">
+                    <button onClick={this.onChange} className="btn btn__increase">Increase</button>
+                    <button className="btn btn__delete">Delete</button>
+                </div>
+            </li>
+        )
+    }
 }
 
 export default AppEmployeeItem;
